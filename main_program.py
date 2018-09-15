6a7efa0695096b4fde6c2b2949ce0f5319ce5694
vaibhavb1996 <vbansal@bu.edu>
2018-09-15 16:53:46 -0400

    added code for downloading images

--- main_program.py
+++ main_program.py
@@ -1,6 +1,10 @@
 import tweetscanner
+import downimg
 
 def main():
 	print("Welcome to my project, Let's get started..")
 	tweetscanner.get_all_tweets(input("Enter the twitter handle: "))
+	print("Let's get the images..")
+	downimg.download_images(tweetscanner.mediaURLs)
+	print("Images downloaded. Converting to video..")
 
