254836b58a6fd0506895b1d152ddc35c7ef8a895
vaibhavb1996 <vbansal@bu.edu>
2018-09-15 16:15:57 -0400

    program for downloading images created

---
+++ downimg.py
@@ -0,0 +1,17 @@
+import os, sys
+import wget
+
+def download_images(mediaURLs):
+	folder=os.getcwd()+"/images"
+	try:
+		if os.path.exists(folder):
+			print("folder already exists")
+		else:
+			os.mkdir(folder)
+	except OSError:
+		print("error creating directory")
+	#downloading files
+	for index,mediaURL in enumerate(mediaURLs):
+            img_no=str(index).zfill(3)
+            img_name=folder+"/"+img_no+".jpg"
+            wget.download(mediaURL, out=img_name)
