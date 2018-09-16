dc0d2a963444eed7bee20119cb3c8331cbae8de5
vaibhavb1996 <vbansal@bu.edu>
2018-09-16 00:01:04 -0400

    added make video call

--- main_program.py
+++ main_program.py
@@ -1,10 +1,14 @@
 import tweetscanner
+import make_video
 
 def main():
 	print("Welcome to my project, Let's get started..")
-	tweetscanner.get_all_tweets(input("Enter the twitter handle: "))
+	handle=input("Enter the twitter handle: ")
+	#calling tweetscanner to get tweets
+	tweetscanner.get_all_tweets(handle)
 	print("Images downloaded. Converting to video..")
-	
+	#calling make video to generate video from images
+	make_video.create_video(handle)
 
 if __name__=='__main__':
 	main()
