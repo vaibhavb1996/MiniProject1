96f35c109dcc55962a64b817243a28bdf54f87a2
vaibhavb1996 <vbansal@bu.edu>
2018-09-18 16:55:55 -0400

    changed twitter credentials source

--- tweetscanner.py
+++ tweetscanner.py
@@ -6,13 +6,13 @@ import tweepy
 import os, sys ,wget
 
 #Twitter Credentials
-#consumerKey="Enter your consumer key"
-#consumerSecret="Enter your consumer secret"
-#accessKey="Enter your access keys"
-#accessSecret="Enter your access secret"
+consumerKey="Enter your consumer key"
+consumerSecret="Enter your consumer secret"
+accessKey="Enter your access keys"
+accessSecret="Enter your access secret"
 
 #access twitter credentials from external file
-import twicred
+#import twicred
 
 def get_all_tweets(screen_name):
 	#verifying credentials
