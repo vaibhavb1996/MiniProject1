80e7a787e77ef69ee180c7c038e6219c89a7e318
vaibhavb1996 <vbansal@bu.edu>
2018-11-30 23:44:58 -0500

    improved formatting

--- analysis.py
+++ analysis.py
@@ -4,18 +4,18 @@ import io
 import sys
 
 def analyse_video(path):
-	video_client=videointelligence.VideoIntelligenceServiceClient()
+	video_client = videointelligence.VideoIntelligenceServiceClient()
 	features = [videointelligence.enums.Feature.LABEL_DETECTION]
 	
 	#input the video to API
 	with io.open(path, 'rb') as video:
-		input_data=video.read()
+		input_data = video.read()
 	#Generation of API result
 	try:
-		operation=video_client.annotate_video(features=features, input_content=input_data)
-		result=operation.result(timeout=90)	
+		operation = video_client.annotate_video(features=features, input_content=input_data)
+		result = operation.result(timeout=90)	
 	except Exception as error:
-		print("Error with video processing: "+error)
+		print("Error with video processing: " + error)
 		exit()
 	print("Analysis is Complete..")
 	
@@ -24,8 +24,8 @@ def analyse_video(path):
 	for i, segment_label in enumerate(segment_labels):
 		print('Video label description: {}'.format(segment_label.entity.description))
 		for category_entity in segment_label.category_entities:
-			print ("Label category description: "+category_entity.description)	
+			print ("Label category description: " + category_entity.description)	
 
-if __name__=='__main__':
+if __name__ == '__main__':
 	print("Not running from Main program..")
 	analyse_video(input("Enter file path: "))
