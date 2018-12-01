3e90fd12a77df3b45756c7ebc8a26bfbbfe85bce
vaibhavb1996 <vbansal@bu.edu>
2018-11-30 23:01:09 -0500

    changed database structure

--- Database.py
+++ Database.py
@@ -16,10 +16,13 @@ db = mysql.connector.connect(
 							)
 # self.cursor = self.db.cursor()
 cursor = db.cursor()
-# cursor.execute("CREATE DATABASE TweetDatabase") !Has to be used just once!
-# cursor.execute("CREATE TABLE Tweethistory(UserID INT, LastUsed DATETIME, Handle VARCHAR(30), TotalImages INT)") #The main table for the data
-cursor.execute("CREATE TABLE Tags(UserID INT, Tag VARCHAR(20))") #Relational table that will contain all the tags generated along with the UserID that encountered them
 
-# cursor.execute("SHOW DATABASES") !Testing if it worked!
+# cursor.execute("CREATE DATABASE TweetDatabase") !Has to be used just once!
+# cursor.execute("CREATE TABLE Tweethistory(UserID INT, LastUsed DATETIME)") #The main table for the data !Only called once!
+# cursor.execute("CREATE TABLE Tags(UserID INT, Handle VARCHAR(20), Tag VARCHAR(20))") #Relational table that will contain all the tags generated along with the UserID that encountered them !Only called onec!
+# cursor.execute("CREATE TABLE Images(UserID INT, Handle VARCHAR(20), Img INT)") #Relational table that will have the images related to each twitter handle 
+# cursor.execute("""ALTER TABLE Tweethistory
+		#		  ADD PRIMARY KEY (UserID)""") #!Added Primary Key to the Tweethistory table
+# cursor.execute("SHOW TABLES") #!Testing if it worked!
 # for x in cursor:
 # 	print(x)
