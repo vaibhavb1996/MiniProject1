b612f4685bc2411267a6b188e46404baaa100216
vaibhavb1996 <vbansal@bu.edu>
2018-12-07 14:20:33 -0500

    added testing lines for mysql

--- test.py
+++ test.py
@@ -2,15 +2,24 @@ from Database import MyDatabase
 from time import gmtime, strftime
 db = MyDatabase()
 try:
-	value = ""
+# 	value = ""
 	time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-	for char in time:
-		value = value + char
-	print(type(value))
-	ID = db.add_user(value)
-	print(ID)
+# 	for char in time:
+# 		value = value + char
+# 	print(type(value))
+# 	# ID = db.add_user(value)
+# 	# print(ID)
+# 	db.update_user(1, time)
 	
-	db.add_images(1, 'BUtweets', 200)
+	# db.add_images(1, 'BUtweets', 200)
+	ID = 2
+	label = 'automotive'
+	handle = 'porsche'
+	check = db.check_user(ID)
+	print(check)
+
+	print(db.update_user(ID,time))
+	db.add_label(ID, handle, label)
 except Exception as e:
 	print(e)
 
