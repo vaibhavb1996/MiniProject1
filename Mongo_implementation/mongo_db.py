575bdc7a08414402bd8779b31c3d6bc5cd05f92e
vaibhavb1996 <vbansal@bu.edu>
2018-12-07 18:36:46 -0500

    added function for the database updates

--- Mongo_implementation/mongo_db.py
+++ Mongo_implementation/mongo_db.py
@@ -8,32 +8,46 @@ myDB = myclient['myDatabase']
 Tweethistory = myDB['Tweethistory']
 
 # Tweethistory.drop()
-mydict = {'_id': 1,
-		  'LastUsed': time,
-		  'usage' : [
-						{
-						'Tags' : ['Metropolitan', 'Cityscape', 'Urban'],
-						'Handle' : 'BUTweets',
-						'Images' : 100
-						},
-						{
-						'Tags' : ['Dogs', 'Labrador'],
-						'Handle' : 'Dogsforlife',
-						'Images' : 100
-						}
-					]
-		 }
-x = Tweethistory.insert_one(mydict)
-print(myclient.list_database_names())
-print(myDB.list_collection_names())
-for x in Tweethistory.find():
-	print(x)
+# mydict = {'_id': 1,
+# 		  'LastUsed': time,
+# 		  'usage' : [
+# 						{
+# 						'Tags' : ['Metropolitan', 'Cityscape', 'Urban'],
+# 						'Handle' : 'BUTweets',
+# 						'Images' : 100
+# 						},
+# 						{
+# 						'Tags' : ['Dogs', 'Labrador'],
+# 						'Handle' : 'Dogsforlife',
+# 						'Images' : 100
+# 						}
+# 					]
+# 		 }
+# x = Tweethistory.insert_one(mydict)
+# print(myclient.list_database_names())
+# print(myDB.list_collection_names())
+# for x in Tweethistory.find():
+# 	print(x)
 
-def add_user(data):
+def check_user(ID):
 	myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 	myDB = myclient['myDatabase']
 	Tweethistory = myDB['Tweethistory']
-	z = Tweethistory.insert_one(data)
+	# check = {'_id': ID}
+	if Tweethistory.find({'_id': ID},{'LastUsed': 0, 'usage': 0}).count() > 0 :
+		return 1
+	else:
+		return 0
+
+def add_user(LastUsed):
+	myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+	myDB = myclient['myDatabase']
+	Tweethistory = myDB['Tweethistory']
+	c = Tweethistory.find_one({},{'LastUsed': 0, 'usage': 0}, sort = [( '_id', pymongo.DESCENDING )]).get('_id') + 1
+	mydict = {'_id':c,
+			  'LastUsed': LastUsed
+			 }
+	z = Tweethistory.insert_one(mydict)
 	return z.inserted_id
 
 def update_user(ID, LastUsed, session):
